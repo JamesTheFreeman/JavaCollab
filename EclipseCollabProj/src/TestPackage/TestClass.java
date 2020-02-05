b19df06a4e1513bfc35f6e74f951434e77b44405
JamesTheFreeman <jameskentthefourth@gmail.com>
2020-02-05 16:57:41 -0500

    Added comments to TestClass.java

--- EclipseCollabProj/src/TestPackage/TestClass.java
+++ EclipseCollabProj/src/TestPackage/TestClass.java
@@ -1,9 +1,16 @@
+/**
+ * @author - YaBoi
+ * This is just a test class w/in a test package. We can create new ones later!
+ */
+
 package TestPackage;
 
 public class TestClass
 {
 	public static void main (String[] args)
 	{
+		// Was just using variations of this to ensure pushing works
 		System.out.println("Test!");
+		// Spoiler: It does!
 	}
 }
