fb69d4a4e631386b7c94123842bb27e58f916aa7
0Phoenix <michaelhelwig2@gmail.com>
2020-02-17 19:07:17 -0500

    Added base code for Game class

--- EclipseCollabProj/src/dungeonCrawlerP1/Game.java
+++ EclipseCollabProj/src/dungeonCrawlerP1/Game.java
@@ -3,8 +3,137 @@
  * Class description here
  */
 
+import java.util.Random;
 package dungeonCrawlerP1;
 
 public class Game {
+    int numOfRooms;
+    RoomNode[] roomArr;
+    int from;
+    Random ran;
 
-}
+    /**
+     * Default Constructor, creates medium room
+     */
+    public Game() {
+        numOfRooms = 0;
+        roomArr = new RoomNode[30];
+        from = 0;
+        ran = new Random();
+    }
+
+    /**
+     * Constructor that allows for map size modification
+     * @param size
+     */
+    public Game(int size) {
+        numOfRooms = 0;
+        roomArr = new RoomNode[size];
+        from = 0;
+        ran = new Random();
+    }
+
+    /**
+     * Dev Constructor for testing
+     */
+    public Game(int numOfRooms, RoomNode[] roomArr, int from) {
+        numOfRooms = this.numOfRooms;
+        roomArr = this.roomArr;
+        from = this.from;
+        ran = new Random();
+    }
+
+    /**
+     * Generate Map based on size
+     * @param size
+     */
+    public void generateMap(int size) {
+        int dir = 0;
+        int plc = 0;
+        for (int i = 1; i <= size; i++) {
+            plc = 1;
+            roomArr[i] = new RoomNode(i,ran.nextInt(20) + 1,ran.nextInt(20) + 1);
+            while (!roomPlaced(roomArr[i])) {
+                dir = ran.nextInt(4) + 1;
+                if (roomDirAvl(roomArr[i],dir)) {
+                    setRoomDir(roomArr[plc],dir,i);
+                    setRoomDir(roomArr[i],opp(dir),i);
+                } else {
+                    plc++;
+                }
+            }
+        }
+    }
+
+    /**
+     * Check to see if the room has been placed, check if n-w are empty
+     * @param x
+     * @return
+     */
+    private boolean roomPlaced(RoomNode x) {
+        boolean res = false;
+        if (x.N != 0 || x.E !=0 || x.S != 0 || x.W !=0) {
+            res = true;
+        }
+        return res;
+    }
+
+    /**
+     * checks to see if a rooms n-w is available
+     * @param x
+     * @param dir
+     * @return
+     */
+    private boolean roomDirAvl(RoomNode x, int dir) {
+        boolean res = false;
+        switch (dir) {
+            case 1:
+                if (x.N == 0) res = true;
+            break;
+            case 2:
+                if (x.E == 0) res = true;
+            break;
+            case 3:
+                if (x.S == 0) res = true;
+            break;
+            case 4:
+                if (x.W == 0) res = true;
+            break;
+        }
+    }
+
+    /**
+     * Set rooms n-w to diffrent rooms index
+     * @param x
+     * @param dir
+     * @param y
+     */
+    private void setRoomDir(RoomNode x,int dir, int y) {
+        switch (dir) {
+            case 1:
+                x.N = y;
+            break;
+            case 2:
+                x.E = y;
+            break;
+            case 3:
+                x.S = y;
+            break;
+            case 4:
+                x.W = y;
+            break;
+         }
+     }
+
+     /**
+      * Change n-w direction to opposite direction
+      * @param dir
+      * @return
+      */
+    private int opp(int dir){
+        if (dir == 1) return 3;
+        if (dir == 2) return 4;
+        if (dir == 3) return 1;
+        if (dir == 4) return 2;
+     }
+ }
