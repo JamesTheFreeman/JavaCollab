fe26220392bcb320546756ff1502c44de68429a2
0Phoenix <michaelhelwig2@gmail.com>
2020-02-18 11:15:40 -0500

    Update Comments and minor code changes

--- EclipseCollabProj/src/dungeonCrawlerP1/Game.java
+++ EclipseCollabProj/src/dungeonCrawlerP1/Game.java
@@ -10,7 +10,7 @@ public class Game {
     int numOfRooms;
     RoomNode[] roomArr;
     int from;
-    Random ran;
+    private Random random;
 
     /**
      * Default Constructor, creates medium room
@@ -19,7 +19,7 @@ public class Game {
         numOfRooms = 0;
         roomArr = new RoomNode[30];
         from = 0;
-        ran = new Random();
+        random = new Random();
     }
 
     /**
@@ -30,7 +30,7 @@ public class Game {
         numOfRooms = size;
         roomArr = new RoomNode[size];
         from = 0;
-        ran = new Random();
+        random = new Random();
     }
 
     /**
@@ -40,7 +40,7 @@ public class Game {
         this.numOfRooms = numOfRooms;
         this.roomArr = roomArr;
         this.from = from;
-        ran = new Random();
+        random = new Random();
     }
 
     /**
@@ -48,35 +48,40 @@ public class Game {
      * @param size
      */
     public void generateMap() {
-        int dir = 0;
-        int plc = 0;
-        int preDir = 0;
+        int direction = 0;
+        int generatorRoom = 0;  // Room thats n-w is being check to see in a room can be generated next to it
+        int pastDirection = 0;
+        // Create number of room based on numOfRooms
         for (int i = 1; i <= numOfRooms; i++) {
-            plc = 1;
-            roomArr[i] = new RoomNode(i,ran.nextInt(20) + 1,ran.nextInt(20) + 1);
+            generatorRoom = 1;
+            roomArr[i] = new RoomNode(i,random.nextInt(20) + 1,random.nextInt(20) + 1);
+            // Repeat until room[i] has a n-w value / is placed
             while (!roomPlaced(roomArr[i])) {
+                // Prevent program to go back to previous generatorRoom
                 do {
-                    dir = ran.nextInt(4) + 1;
-                } while (dir == preDir);
-                if (roomDirAvl(roomArr[plc],dir)) {
-                    setRoomDir(roomArr[plc],dir,i);
-                    setRoomDir(roomArr[i],opp(dir),i);
+                    direction = random.nextInt(4) + 1;
+                } while (direction == pastDirection);
+                // Check if generatorRooms n-w is available
+                if (roomDirAvl(roomArr[generatorRoom],direction)) {
+                    setRoomDir(roomArr[generatorRoom],direction,i);
+                    setRoomDir(roomArr[i],opp(direction),i);
                 } else {
-                    switch (dir) {
+                    // else go to next generator room based on n-w
+                    switch (direction) {
                         case 1:
-                            plc = roomArr[plc].N;
+                            generatorRoom = roomArr[generatorRoom].N;
                             break;
                         case 2:
-                            plc = roomArr[plc].E;
+                            generatorRoom = roomArr[generatorRoom].E;
                             break;
                         case 3:
-                            plc = roomArr[plc].S;
+                            generatorRoom = roomArr[generatorRoom].S;
                             break;
                         case 4:
-                            plc = roomArr[plc].W;
+                            generatorRoom = roomArr[generatorRoom].W;
                             break;
                     }
-                    preDir = dir;
+                    pastDirection = direction;
                 }
             }
         }
@@ -84,74 +89,77 @@ public class Game {
 
     /**
      * Check to see if the room has been placed, check if n-w are empty
-     * @param x
+     * @param roomNode
      * @return
      */
-    private boolean roomPlaced(RoomNode x) {
-        boolean res = false;
-        if (x.N != 0 || x.E !=0 || x.S != 0 || x.W !=0) {
-            res = true;
+    private boolean roomPlaced(RoomNode roomNode) {
+        if (roomNode.N != 0 || roomNode.E !=0 || roomNode.S != 0 || roomNode.W !=0) {
+            return true;
         }
-        return res;
+        return false;
     }
 
     /**
      * checks to see if a rooms n-w is available
-     * @param x
-     * @param dir
+     * @param roomNode
+     * @param direction
      * @return
      */
-    private boolean roomDirAvl(RoomNode x, int dir) {
-        boolean res = false;
-        switch (dir) {
+    private boolean roomDirAvl(RoomNode roomNode, int direction) {
+        switch (direction) {
             case 1:
-                if (x.N == 0) res = true;
-                break;
+                if (roomNode.N == 0) return true;
             case 2:
-                if (x.E == 0) res = true;
-                break;
+                if (roomNode.E == 0) return true;
             case 3:
-                if (x.S == 0) res = true;
-                break;
+                if (roomNode.S == 0) return true;
             case 4:
-                if (x.W == 0) res = true;
-                break;
+                if (roomNode.W == 0) return true;
+            default:
+                return false;
         }
-        return res;
     }
 
     /**
      * Set rooms n-w to diffrent rooms index
-     * @param x
-     * @param dir
-     * @param y
+     * @param roomNode
+     * @param direction
+     * @param input
      */
-    private void setRoomDir(RoomNode x,int dir, int y) {
-        switch (dir) {
+    private void setRoomDir(RoomNode roomNode,int direction, int input) {
+        switch (direction) {
             case 1:
-                x.N = y;
+                roomNode.N = input;
                 break;
             case 2:
-                x.E = y;
+                roomNode.E = input;
                 break;
             case 3:
-                x.S = y;
+                roomNode.S = input;
                 break;
             case 4:
-                x.W = y;
+                roomNode.W = input;
                 break;
          }
      }
 
      /**
-      * Change n-w direction to opposite direction
-      * @param dir
+      * Change n-w direction to opposite direction, If error throw -1
+      * @param direction
       * @return
       */
-    private int opp(int dir){
-        if (dir == 1) return 3;
-        if (dir == 2) return 4;
-        if (dir == 3) return 1;
-        return 2;
+    private int opp(int direction){
+        switch (direction) {
+            case 1:
+                return 3;
+            case 2:
+                return 4;
+            case 3: 
+                return 1;
+            case 4: 
+                return 2;
+            default:
+                return -1;
+        }
      }
  }
