c4cecd47917764a20717e4a250a7c6858662e870
JamesTheFreeman <jameskentthefourth@gmail.com>
2020-03-05 11:55:48 -0500

    Some prep on CombatPhase, still need to framework how combat works

--- EclipseCollabProj/src/dungeonCrawlerP1/CombatPhase.java
+++ EclipseCollabProj/src/dungeonCrawlerP1/CombatPhase.java
@@ -6,17 +6,58 @@
 
 package dungeonCrawlerP1;
 
+import java.util.Random;
+
 public class CombatPhase
 {
-	private int enemyType;
+	private int enemyDiff;	// Difficulty of enemy encounter
+	private int enemyNum;	// Number of enemies (Max of 5)
+	
+	private boolean tracing = false;
 	
 	/**
-	 * Default constructor, takes one parameter
-	 * @param x		Integer representing enemy type
+	 * Two parameter constructor, sets enemy difficulty & number of enemies
+	 * @param range		Max enemy # to generate enemy from
+	 * @param num		Number of enemies to generate
+	 * Also sets number of enemies to 5 if it exceeds the intended limit
 	 */
-	public CombatPhase(int x)
+	public CombatPhase(int range, int num)
 	{
-		enemyType = x;
+		enemyDiff = range;
+		enemyNum = num;
+		if (num > 5) enemyNum = 5;
+	}
+	/**
+	 * One parameter constructor, sets enemy difficulty & defaults num to 1
+	 * @param range		Max enemy # to generate enemy from
+	 */
+	public CombatPhase(int range)
+	{
+		enemyDiff = range;
+		enemyNum = 1;
+	}
+	/**
+	 * Two parameter w/ option for tracing
+	 */
+	public CombatPhase(int range, int num, boolean trce)
+	{
+		enemyDiff = range;
+		enemyNum = num;
+		if (num > 5) enemyNum = 5;
+		tracing = true;
+		
+		System.out.println("Tracing for combat enabled");
+	}
+	/**
+	 * One parameter w/ option for tracing
+	 */
+	public CombatPhase(int range, boolean trce)
+	{
+		enemyDiff = range;
+		enemyNum = 1;
+		tracing = true;
+		
+		System.out.println("Tracing for combat enabled");
 	}
 	
 	// METHODS
@@ -29,12 +70,34 @@ public class CombatPhase
 	 */
 	public void initializeFight(String pInput, Game g, Player p)
 	{
-		switch(enemyType)
+		Random enemyRand = new Random();
+		int enemyType, count = 0;
+		
+		// Creates new enemies (as applicable)
+		switch (enemyNum)
 		{
-			// Use to construct objects of type Enemy
-			// Stats can be pulled from txt (a la roomtypes.txt)
-			// along with any physical description(s) of that enemy
+			case 5:
+				enemyType = enemyRand.nextInt(enemyDiff) + 1;
+				Enemy enemy5 = new Enemy(enemyType);
+				count++;
+			case 4:
+				enemyType = enemyRand.nextInt(enemyDiff) + 1;
+				Enemy enemy4 = new Enemy(enemyType);
+				count++;
+			case 3:
+				enemyType = enemyRand.nextInt(enemyDiff) + 1;
+				Enemy enemy3 = new Enemy(enemyType);
+				count++;
+			case 2:
+				enemyType = enemyRand.nextInt(enemyDiff) + 1;
+				Enemy enemy2 = new Enemy(enemyType);
+				count++;
+			case 1:
+				enemyType = enemyRand.nextInt(enemyDiff) + 1;
+				Enemy enemy1 = new Enemy(enemyType);
+				count++;
 		}
+		if (tracing) System.out.println(count + " enemies generated");
 		
 		pInput.toLowerCase();
 		switch(pInput)
